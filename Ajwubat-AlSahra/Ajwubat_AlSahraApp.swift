190ea77cd2972d55eff2b059d71a3273cab796b0
Lemon <101458752+LemonLabours@users.noreply.github.com>
2023-09-19 14:24:22 +0300

    Initial Commit

---
+++ Ajwubat-AlSahra/Ajwubat_AlSahraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ajwubat_AlSahraApp.swift
+//  Ajwubat-AlSahra
+//
+//  Created by Lama AL Yousef on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Ajwubat_AlSahraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
