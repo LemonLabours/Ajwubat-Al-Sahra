ea15bbbb941532f08ed3226cf1f05907bba96891
Lemon <101458752+LemonLabours@users.noreply.github.com>
2023-09-20 15:39:20 +0300

    picker training

--- Ajwubat-AlSahra/ContentView.swift
+++ Ajwubat-AlSahra/ContentView.swift
@@ -8,43 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var  tapCount = 0
+    @State private var tapCount = 0
     @State private var name = ""
-    let students = ["nona", "Loli" , "toto"]
+    let students = ["nona", "Loli", "toto"]
     @State private var selectedStudent = "nona"
 
-
     var body: some View {
-        VStack {
-            NavigationView{
-                Form{
-                    
-                    Section{
-                        TextField("Enter your name" , text: $name)
-                        
-                        Text("Hello, \(name)")
-                        
-                    }
-                    Group{
-                        ForEach(0..<3){
-                            Text("Row \($0)")
-                        }
-                    }
-                    Group{
-                        Button("Tap Count \(tapCount)"){
-                            tapCount += 1
-                        }
-                    }
-                    Section{
-                        Picker("Select a Student", selection: $selectedStudent)
-                        
-                    }}
-                    }
-                }.navigationTitle("SwiftUi")
-                    .navigationBarTitleDisplayMode(.inline)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter your name", text: $name)
+                    Text("Hello, \(name)")
+                }
 
+                ForEach(0..<3) {
+                    Text("Row \($0)")
+                }
+
+                Button("Tap Count \(tapCount)") {
+                    tapCount += 1
+                }
+
+                Section {
+                    Picker("Select a Student", selection: $selectedStudent) {
+                        ForEach(students, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
             }
-            
+            .navigationTitle("SwiftUi")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
@@ -53,3 +48,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
