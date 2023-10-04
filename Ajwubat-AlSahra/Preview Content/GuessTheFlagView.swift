da6d1550ce06cd351d502a03d043abf6cf0f6fc4
Lemon <101458752+LemonLabours@users.noreply.github.com>
2023-10-04 09:34:21 +0300

    NEW START

---
+++ Ajwubat-AlSahra/Preview Content/GuessTheFlagView.swift
@@ -0,0 +1,18 @@
+//
+//  GuessTheFlag.swift
+//  Ajwubat-AlSahra
+//
+//  Created by Lama AL Yousef on 26/09/2023.
+//
+
+import SwiftUI
+
+struct GuessTheFlagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuessTheFlagView()
+}
